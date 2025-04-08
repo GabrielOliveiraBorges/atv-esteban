771e98d933c1bb41a2d159367244945ae11b054d
Gabriel <Gabriel@DESKTOP-OFSS190>
2025-04-08 18:24:47 -0300

    Add project files.

---
+++ atv esteban/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static int BuscaSequencial(int[] array, int elemento)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == elemento)
+            {
+                return i; 
+            }
+        }
+        return -1;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] numeros = { 10, 20, 30, 40, 50 };
+        int elementoBuscado = 30;
+
+        int indice = BuscaSequencial(numeros, elementoBuscado);
+
+        if (indice != -1)
+        {
+            Console.WriteLine($"Elemento {elementoBuscado} encontrado no índice {indice}.");
+        }
+        else
+        {
+            Console.WriteLine("Elemento não encontrado.");
+        }
+    }
+}
